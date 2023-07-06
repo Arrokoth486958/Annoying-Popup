b19576450e53bd7c872f7a5572a7037125adc325
Arrokoth <arrokoth233@gmail.com>
2023-07-06 19:14:43 +0200

    移出了Logger

--- src/main/java/dev/arrokoth/annoying_popup/AnnoyingPopup.java
+++ src/main/java/dev/arrokoth/annoying_popup/AnnoyingPopup.java
@@ -1,14 +1,12 @@
 package dev.arrokoth.annoying_popup;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
 
 @Mod(AnnoyingPopup.MOD_ID)
 public class AnnoyingPopup {
     public static final String MOD_ID = "annoying_popup";
-    private static final Logger LOGGER = LogUtils.getLogger();
+//    private static final Logger LOGGER = LogUtils.getLogger();
 
     public AnnoyingPopup() {
         MinecraftForge.EVENT_BUS.register(this);
