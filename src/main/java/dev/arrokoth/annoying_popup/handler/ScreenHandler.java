55d3a42b7b6fd99c298083d738849261f7a7d4a0
Arrokoth <arrokoth233@gmail.com>
2023-07-06 19:10:35 +0200

    更烦力（悲

--- src/main/java/dev/arrokoth/annoying_popup/handler/ScreenHandler.java
+++ src/main/java/dev/arrokoth/annoying_popup/handler/ScreenHandler.java
@@ -1,19 +1,27 @@
-package dev.arrokoth.AnnoyingPopup.handler;
+package dev.arrokoth.annoying_popup.handler;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.arrokoth.annoying_popup.AnnoyingPopup;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
+import net.minecraft.client.gui.screens.worldselection.SelectWorldScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.awt.*;
-import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -26,20 +34,32 @@ import java.util.Objects;
 @Mod.EventBusSubscriber(modid = AnnoyingPopup.MOD_ID, value = Dist.CLIENT)
 public class ScreenHandler {
     public static final String COUNTRY = Locale.getDefault().getCountry();
-    public static final int TEXT_COLOR = new Color(127, 127, 127).getRGB();
-    public static final int HYPERLINK_COLOR = new Color(63, 127, 255).getRGB();
+    public static final int TEXT_COLOR = new Color(0, 0, 0).getRGB();
+    public static final int HYPERLINK_COLOR = new Color(63, 63, 255).getRGB();
     private static boolean isOpening = false;
+
+    public static boolean shouldOpen(Screen screen) {
+        return screen instanceof TitleScreen ||
+                screen instanceof SelectWorldScreen ||
+                screen instanceof JoinMultiplayerScreen;
+    }
 
     @SubscribeEvent
     public static void init(final ScreenEvent.Init event) {
-        if (event.getScreen() instanceof TitleScreen && Objects.equals(COUNTRY, "CN")){
+        System.out.println(event.getScreen().getClass());
+        if (shouldOpen(event.getScreen()) && Objects.equals(COUNTRY, "CN")){
             isOpening = true;
         }
     }
 
+    private static void drawCenteredString(GuiGraphics graphics, Font p_282901_, Component p_282456_, int p_283083_, int p_282276_, int p_281457_) {
+        FormattedCharSequence formattedcharsequence = p_282456_.getVisualOrderText();
+        graphics.drawString(p_282901_, formattedcharsequence, p_283083_ - p_282901_.width(formattedcharsequence) / 2, p_282276_, p_281457_, false);
+    }
+
     @SubscribeEvent
     public static void render(final ScreenEvent.Render event) {
-        if (event.getScreen() instanceof TitleScreen && isOpening) {
+        if (shouldOpen(event.getScreen()) && isOpening) {
             RenderSystem.assertOnRenderThread();
 
             RenderSystem.enableBlend();
@@ -56,7 +76,7 @@ public class ScreenHandler {
             String[] split = text.getString().split("\n");
             for (int i = 0; i < split.length; i++) {
                 String part = split[i];
-                event.getGuiGraphics().drawCenteredString(Minecraft.getInstance().font, Component.literal(part), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 - 232 / 2 + 72 + 12 * i, TEXT_COLOR);
+                drawCenteredString(event.getGuiGraphics(), Minecraft.getInstance().font, Component.literal(part), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 - 232 / 2 + 72 + 12 * i, TEXT_COLOR);
             }
 
             if (event.getMouseX() >= Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 128 / 2 && event.getMouseX() <= Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 + 128 * 2 &&
@@ -66,13 +86,13 @@ public class ScreenHandler {
                 event.getGuiGraphics().blit(new ResourceLocation("annoying_popup", "textures/gui/button.png"), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 128 / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 76, 0, 0, 128, 32, 128, 32);
             }
 
-            event.getGuiGraphics().drawCenteredString(Minecraft.getInstance().font, Component.translatable("popup.info"), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 32, TEXT_COLOR);
+            drawCenteredString(event.getGuiGraphics(), Minecraft.getInstance().font, Component.translatable("popup.info"), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 32, TEXT_COLOR);
 
             if (event.getMouseX() >= Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 72 / 2 && event.getMouseX() <= Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 + 72 / 2 &&
                     event.getMouseY() >= Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 20 && event.getMouseY() <= Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 20 + 8) {
-                event.getGuiGraphics().drawCenteredString(Minecraft.getInstance().font, Component.translatable("popup.hyperlink").setStyle(Style.EMPTY.withUnderlined(true)), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 20, HYPERLINK_COLOR);
+                drawCenteredString(event.getGuiGraphics(), Minecraft.getInstance().font, Component.translatable("popup.hyperlink").setStyle(Style.EMPTY.withUnderlined(true)), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 20, HYPERLINK_COLOR);
             } else {
-                event.getGuiGraphics().drawCenteredString(Minecraft.getInstance().font, Component.translatable("popup.hyperlink"), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 20, TEXT_COLOR);
+                drawCenteredString(event.getGuiGraphics(), Minecraft.getInstance().font, Component.translatable("popup.hyperlink"), Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2, Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 20, TEXT_COLOR);
             }
         }
     }
@@ -82,23 +102,11 @@ public class ScreenHandler {
         if (isOpening && event.isCancelable()) {
             if (event.getMouseX() >= (double) (Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 128 / 2) && event.getMouseX() <= (double) (Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 + 128 * 2) &&
                     event.getMouseY() >= (double) (Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 76) && event.getMouseY() <= (double) (Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 76 + 32)) {
-                System.out.println(114514);
                 try {
-                    if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-                        Runtime.getRuntime().exec(new String[] {"explorer", "https://mc.163.com/"});
-                    } else {
-                        Runtime.getRuntime().exec(new String[] {"open", "https://mc.163.com/"});
-                    }
-                } catch (IOException e) {
+                    Util.getPlatform().openUri(new URI("https://mc.163.com/"));
+                } catch (URISyntaxException e) {
                     throw new RuntimeException(e);
                 }
-//                if (Desktop.isDesktopSupported()) {
-//                    try {
-//                        Desktop.getDesktop().browse(new URI("https://mc.163.com/"));
-//                    } catch (Exception e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                }
             }
             if (event.getMouseX() >= (double) (Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 72 / 2) && event.getMouseX() <= (double) (Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 + 72 / 2) &&
                     event.getMouseY() >= (double) (Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 20) && event.getMouseY() <= (double) (Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 + 232 / 2 - 20 + 8)) {
@@ -110,21 +118,21 @@ public class ScreenHandler {
 
     @SubscribeEvent
     public static void mouseButtonReleased(final ScreenEvent.MouseButtonReleased event) {
-        if (isOpening && event.isCancelable()) {
+        if (shouldOpen(event.getScreen()) && isOpening && event.isCancelable()) {
             event.setCanceled(true);
         }
     }
 
     @SubscribeEvent
     public static void keyPressed(final ScreenEvent.KeyPressed event) {
-        if (isOpening && event.isCancelable()) {
+        if (shouldOpen(event.getScreen()) && isOpening && event.isCancelable()) {
             event.setCanceled(true);
         }
     }
 
     @SubscribeEvent
     public static void keyReleased(final ScreenEvent.KeyReleased event) {
-        if (isOpening && event.isCancelable()) {
+        if (shouldOpen(event.getScreen()) && isOpening && event.isCancelable()) {
             event.setCanceled(true);
         }
     }
